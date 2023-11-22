368a59ef07de2760f9b6fd0559f2c3f556292e78
Max van der Heijden <max123vdh@gmail.com>
2023-11-22 12:32:25 +0100

    back to optic

--- plansys2_optic_plan_solver-main/include/plansys2_optic_plan_solver/optic_plan_solver.hpp
+++ plansys2_optic_plan_solver-main/include/plansys2_optic_plan_solver/optic_plan_solver.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Intelligent Robotics Lab
+// Copyright 2021 PySC21
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,46 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef PLANSYS2_UPF_PLAN_SOLVER__UPF_PLAN_SOLVER_HPP_
-#define PLANSYS2_UPF_PLAN_SOLVER__UPF_PLAN_SOLVER_HPP_
+#ifndef PLANSYS2_OPTIC_PLAN_SOLVER__OPTIC_PLAN_SOLVER_HPP_
+#define PLANSYS2_OPTIC_PLAN_SOLVER__OPTIC_PLAN_SOLVER_HPP_
 
 #include <optional>
 #include <memory>
 #include <string>
 
 #include "plansys2_core/PlanSolverBase.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
-#include "upf_msgs/action/pddl_plan_one_shot.hpp"
 
 namespace plansys2
 {
 
-class UPFPlanSolver : public PlanSolverBase
+class OPTICPlanSolver : public PlanSolverBase
 {
-private:
-  using PDDLPlanOneShot = upf_msgs::action::PDDLPlanOneShot;
-
-  std::string parameter_name_;
-  rclcpp_lifecycle::LifecycleNode::SharedPtr lc_node_;
-  rclcpp::Node::SharedPtr service_node_;
-
-  plansys2_msgs::msg::Plan convert(const upf_msgs::msg::Plan & plan);
-  plansys2_msgs::msg::PlanItem convert(const upf_msgs::msg::ActionInstance & action);
-
 public:
-  UPFPlanSolver();
+  OPTICPlanSolver();
 
-  void configure(rclcpp_lifecycle::LifecycleNode::SharedPtr &, const std::string &);
+  void configure(rclcpp_lifecycle::LifecycleNode::SharedPtr &, const std::string &) {}
 
-  std::optional<plansys2_msgs::msg::Plan> getPlan(
+  std::optional<Plan> getPlan(
     const std::string & domain, const std::string & problem,
     const std::string & node_namespace = "");
 
-  bool is_valid_domain(
+  std::string isDomainValid(
     const std::string & domain,
     const std::string & node_namespace = "");
 };
 
 }  // namespace plansys2
 
-#endif  // PLANSYS2_UPF_PLAN_SOLVER__UPF_PLAN_SOLVER_HPP_
+#endif  // PLANSYS2_OPTIC_PLAN_SOLVER__OPTIC_PLAN_SOLVER_HPP_
